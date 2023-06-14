632ae4b02eda53f124bc4721870e9d186a00c6b3
김 승호 <naho199345@jinhaksa.net>
2023-06-15 08:39:46 +0900

    delete: index.ts

--- src/common/utils/providers/index.ts
+++ src/common/utils/providers/index.ts
@@ -1,6 +1,4 @@
 export { Logger } from '@nestjs/common';
 
 export * from './config.service';
-export * from './crypto.service';
-export * from './mssql/db.service';
 export * from './xlsx/xlsx.service';
